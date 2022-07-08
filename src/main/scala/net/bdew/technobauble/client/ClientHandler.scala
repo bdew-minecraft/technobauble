d1845056f12f224b8969e650738cc3373a9d90e1
bdew <blackdew@gmail.com>
2022-07-08 13:49:10 +0300

    update for forge rendering rework

--- src/main/scala/net/bdew/technobauble/client/ClientHandler.scala
+++ src/main/scala/net/bdew/technobauble/client/ClientHandler.scala
@@ -2,19 +2,18 @@ package net.bdew.technobauble.client
 
 import net.bdew.technobauble.Technobauble
 import net.minecraftforge.api.distmarker.Dist
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 
 @Mod.EventBusSubscriber(value = Array(Dist.CLIENT), modid = Technobauble.ModId, bus = Bus.MOD)
 object ClientHandler {
   @SubscribeEvent
-  def clientSetup(ev: FMLClientSetupEvent): Unit = {
-    ev.enqueueWork(new Runnable {
-      override def run(): Unit = {
-        Keybinds.init()
-      }
-    })
+  def registerKeybinds(ev: RegisterKeyMappingsEvent): Unit = {
+    ev.register(Keybinds.openBackpack)
+    ev.register(Keybinds.toggleMagnet)
+    ev.register(Keybinds.toggleRun)
+    ev.register(Keybinds.toggleJump)
   }
 }
--- src/main/scala/net/bdew/technobauble/client/Keybinds.scala
+++ src/main/scala/net/bdew/technobauble/client/Keybinds.scala
@@ -1,14 +1,18 @@
 package net.bdew.technobauble.client
 
 import com.mojang.blaze3d.platform.InputConstants
+import net.bdew.technobauble.Technobauble
 import net.bdew.technobauble.network.{ActivateKind, MsgClientActivate, NetworkHandler}
 import net.minecraft.client.KeyMapping
-import net.minecraftforge.client.ClientRegistry
+import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.client.event.InputEvent
 import net.minecraftforge.client.settings.{KeyConflictContext, KeyModifier}
-import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus
 import org.lwjgl.glfw.GLFW
 
+@Mod.EventBusSubscriber(value = Array(Dist.CLIENT), modid = Technobauble.ModId, bus = Bus.FORGE)
 object Keybinds {
   val openBackpack = new KeyMapping(
     "technobauble.key.backpack",
@@ -42,18 +46,11 @@ object Keybinds {
     "technobauble.name"
   )
 
-  private def onKeyInput(event: InputEvent.KeyInputEvent): Unit = {
+  @SubscribeEvent
+  def onKeyInput(event: InputEvent.Key): Unit = {
     if (openBackpack.isDown) NetworkHandler.sendToServer(MsgClientActivate(ActivateKind.BACKPACK))
     if (toggleMagnet.isDown) NetworkHandler.sendToServer(MsgClientActivate(ActivateKind.TOGGLE_MAGNET))
     if (toggleRun.isDown) NetworkHandler.sendToServer(MsgClientActivate(ActivateKind.TOGGLE_RUN))
     if (toggleJump.isDown) NetworkHandler.sendToServer(MsgClientActivate(ActivateKind.TOGGLE_JUMP))
-  }
-
-  def init(): Unit = {
-    ClientRegistry.registerKeyBinding(openBackpack)
-    ClientRegistry.registerKeyBinding(toggleMagnet)
-    ClientRegistry.registerKeyBinding(toggleRun)
-    ClientRegistry.registerKeyBinding(toggleJump)
-    MinecraftForge.EVENT_BUS.addListener(this.onKeyInput)
   }
 }
